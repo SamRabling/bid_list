6dde34dbcc1c531300b1f50688fba3ed00f487e5
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-21 17:14:57 -0700

    component set up

--- AngularApp/src/app/app-routing.module.ts
+++ AngularApp/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ArtWorksComponent } from './art-works/art-works.component';
+import { HomeComponent } from './home/home.component';
+import { ShowPieceComponent } from './show-piece/show-piece.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'user', component: UserDashboardComponent },
+  { path: 'user/wishlist/:id', component: WishlistComponent },
+  { path: 'artworks', component: ArtWorksComponent },
+  { path: 'artworks/:id', component: ShowPieceComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpService } from './http.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +10,7 @@ import { ShowPieceComponent } from './show-piece/show-piece.component';
 import { HomeComponent } from './home/home.component';
 import { ArtWorksComponent } from './art-works/art-works.component';
 import { WishlistComponent } from './wishlist/wishlist.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 
 @NgModule({
   declarations: [
@@ -14,13 +18,16 @@ import { WishlistComponent } from './wishlist/wishlist.component';
     ShowPieceComponent,
     HomeComponent,
     ArtWorksComponent,
-    WishlistComponent
+    WishlistComponent,
+    UserDashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
